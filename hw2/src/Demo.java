b65d40ac3e0b75abad3ca6d2ae59b0df4237f04e
Yury Krivinets <yura_r37@bk.ru>
2018-01-28 17:45:36 +0200

    hw2 update

--- hw2/src/Demo.java
+++ hw2/src/Demo.java
@@ -11,31 +11,36 @@ import java.util.concurrent.TimeUnit;
 public class Demo {
 	private static final Path FILE = Paths.get("output.txt");
 	private static final String STOP_WORD = "quit";
-	private static ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+
+	private static StringBuilder sb = new StringBuilder();
+
 	private static volatile String input;
 	private static String prevInput;
 
 	public static void writeValue() {
 		if (input != null && !input.equals(prevInput)) {
 			try {
-				Files.write(FILE, input.getBytes(), FILE.toFile().exists() ? StandardOpenOption.TRUNCATE_EXISTING : StandardOpenOption.CREATE_NEW);
+				sb.append(input).append(System.getProperty("line.separator"));
+				Files.write(FILE, sb.toString().getBytes(), prevInput != null ? StandardOpenOption.CREATE : StandardOpenOption.TRUNCATE_EXISTING);
 				prevInput = input;
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			if (STOP_WORD.equals(input)) {
-				exec.shutdown();
-			}
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Scanner sc = new Scanner(System.in);
+		ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
 		exec.scheduleAtFixedRate(Demo::writeValue, 1, 1, TimeUnit.SECONDS);
 
 		do {
 			System.out.print("Please enter line: ");
 			input = sc.nextLine();
 		} while (!STOP_WORD.equals(input));
+
+		Thread.sleep(1000);
+		exec.shutdown();
+		exec.awaitTermination(1, TimeUnit.SECONDS);
 	}
 }
